00ee7d00d4a258b98c11eaf526471ef4b9bb9673
Présentation <test@test.com>
2022-10-31 12:50:20 +0100

    date & time integration

--- logger/src/main/test/fr/epita/TestDate.java
+++ logger/src/main/test/fr/epita/TestDate.java
@@ -1,20 +1,37 @@
 package fr.epita;
 
 import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
 public class TestDate {
 
     public static void main(String[] args) {
        // javaUtilDateTest();
-
+        //"date to String"
         LocalDateTime dateTime = LocalDateTime.now();
         System.out.println(dateTime);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HH:mm:ss.SSS");
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd - HH:mm:ss.SSS");
         System.out.println(formatter.format(dateTime));
+
+
+        //"String to date"
+        String input = "m014/05/25 - 16:58:10.302";
+
+        try {
+            TemporalAccessor parsedDateTime = formatter.parse(input);
+            System.out.println(parsedDateTime);
+        }catch (DateTimeException e){
+            System.out.println("error in the format, please correct the following input :"+ input);
+            e.printStackTrace();
+        } finally {
+
+        }
+
+
     }
 
 
@@ -22,7 +39,6 @@ public class TestDate {
     private static void javaUtilDateTest() {
         Date date = new Date();
         System.out.println(date);
-
         //year-month-day-hours-minutes-seconds-milliseconds
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd-HH:mm:ss.SSS");
         String formattedDate = simpleDateFormat.format(date);
