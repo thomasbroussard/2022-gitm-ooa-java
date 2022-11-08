adf5334070ab1d07d00e432992d8545c9116cbed
Thomas <thomas.broussard@gmail.com>
2022-11-08 13:08:42 +0100

    Csv reader in main

---
+++ csv-reader/src/fr/epita/services/CSVReader.java
@@ -0,0 +1,19 @@
+package fr.epita.services;
+
+import fr.epita.datamodel.Person;
+
+import java.io.File;
+import java.util.List;
+
+public class CSVReader {
+
+
+    public CSVReader(File file){
+
+    }
+
+    public List<Person> readPersons(){
+
+    }
+
+}
