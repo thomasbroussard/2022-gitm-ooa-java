eec0e70738b851539074bf33602bd8d65c79338f
Thomas <thomas.broussard@gmail.com>
2022-11-15 12:40:06 +0100

    * another example with bio stat data

--- csv-reader/test/fr/epita/csv/tests/TestCsvLoading.java
+++ csv-reader/test/fr/epita/csv/tests/TestCsvLoading.java
@@ -10,8 +10,17 @@ import java.util.List;
 public class TestCsvLoading {
 
     public static void main(String[] args) throws FileNotFoundException {
+        //
         CSVReader reader = new CSVReader(new File("test.csv"));
         List<Person> persons = reader.readPersons();
         System.out.println(persons);
+
+        if (persons.size() != 3){
+            throw new AssertionError("expected size 3, but is actually "+ persons.size());
+        }
+        if (!persons.get(2).getName().equalsIgnoreCase("julia")){
+            throw new AssertionError("expected 3rd person to be julia, but got "+ persons.get(2).getName());
+        }
+
     }
 }
