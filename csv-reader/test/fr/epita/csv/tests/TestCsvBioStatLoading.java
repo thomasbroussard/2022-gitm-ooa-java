ab3edbff50dfb8100428668434080597002f5d19
Thomas <thomas.broussard@gmail.com>
2022-11-15 13:12:16 +0100

    Biostat data

--- csv-reader/test/fr/epita/csv/tests/TestCsvBioStatLoading.java
+++ csv-reader/test/fr/epita/csv/tests/TestCsvBioStatLoading.java
@@ -7,16 +7,31 @@ import fr.epita.services.CSVReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.OptionalDouble;
 
 public class TestCsvBioStatLoading {
 
     public static void main(String[] args) throws FileNotFoundException {
         //
         CSVBioStatReader reader = new CSVBioStatReader(new File("freshman_kgs.csv"));
-        List<BioStatData> bioStatData = reader.readBioStatData();
-        System.out.println(bioStatData);
+        List<BioStatData> bioStatDataList = reader.readBioStatData();
+        System.out.println(bioStatDataList);
 
+        int total = 0;
+        for (BioStatData bioStatData : bioStatDataList){
+            Integer weightSeptember = bioStatData.getWeightSeptember();
+            total = total + weightSeptember;
+        }
+        Double average = (double) (total / bioStatDataList.size());
+        System.out.println(average);
+
+        Double average2 = bioStatDataList.stream()
+                .mapToInt(BioStatData::getWeightSeptember)
+                .average()
+                .getAsDouble();
+        System.out.println(average2);
 
     }
 }
