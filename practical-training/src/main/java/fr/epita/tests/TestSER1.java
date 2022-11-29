261d1909ab01ffc78416b87a055208c03cc11d85
Thomas <thomas.broussard@gmail.com>
2022-11-29 11:42:01 +0100

    Practical first commit

---
+++ practical-training/src/main/java/fr/epita/tests/TestSER1.java
@@ -0,0 +1,14 @@
+package fr.epita.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class TestSER1 {
+
+    public void test() throws IOException {
+        List<String> lines = Files.readAllLines(new File("data.csv").toPath());
+        System.out.println(lines.get(2));
+    }
+}
